b1ab1fcd33e97b5f27755c3ee63fe4510a3cfd09
Vicram Rajagopalan <42707603+vicramr@users.noreply.github.com>
2021-11-22 15:58:38 -0600

    Added filtering to merge.py Also made it take paths at the command line and made it read the file using read_excel instead of read_csv.

--- data-cleaning-python/merge.py
+++ data-cleaning-python/merge.py
@@ -1,12 +1,8 @@
+import sys
 import os
 import re
 import pandas as p
 from pydub import AudioSegment
-excelfile = '10112_R2_CSV.csv'
-audios_dir = '10112_R2/'
-audio_files = os.listdir(audios_dir)
-df = p.read_csv(excelfile)
-
 
 
 def find_file(filenames, begin, end):
@@ -14,7 +10,7 @@ def find_file(filenames, begin, end):
     filenames: list of filenames
     begin: first number to look for in filename
     end: second number to look for in filename
-    return: the first filename that contains both begin and end
+    return: the first filename that contains both begin and end, or None if it doesn't exist
     """
     filenames = sorted(filenames)
     pattern = re.compile(r'.(\d+)-.(\d+)') # TODO only works with current filename formatting
@@ -26,7 +22,8 @@ def find_file(filenames, begin, end):
             continue
         if b_str == m.group(1) and e_str == m.group(2):
             return name
-    raise ValueError('file not found')
+    return None
+
 def get_begin_end(timepoint):
     """
     timepoint: string from timepoint column
@@ -51,17 +48,34 @@ def get_begin_end(timepoint):
         end = 60 * int(endtime_split[0]) + int(endtime_split[1])
     return begin, end
 
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        sys.exit('Expected two paths as cmd-line args: excelfile, audios_dir')
+    excelfile = sys.argv[1]
+    audios_dir = sys.argv[2]
+    audio_files = os.listdir(audios_dir)
+    df = p.read_excel(excelfile)
 
-audios = []
-debug = True
-for tp in df['Timepoint']:
-    b, e = get_begin_end(tp)
-    fname = find_file(audio_files, b, e)
-    if debug:
-        print('tp:', tp, '\tfname:', fname, '\tb:', b, '\te:', e)
-    audio = AudioSegment.from_wav(os.path.join(audios_dir, fname))
-    # NOTE: if file fails to load, pydub handles it by truncating audio data
-    # print('raw bytes loaded:', audio.raw_data)
-    audios.append(audio)
-df['Audio Clip'] = audios
-print('done')
+    # Load file for each row, create new column, add that column
+    audios = []
+    debug = True
+    for tp in df['Timepoint']:
+        b, e = get_begin_end(tp)
+        fname = find_file(audio_files, b, e)
+        if debug:
+            print('tp:', tp, '\tfname:', fname, '\tb:', b, '\te:', e)
+        if fname is None:
+            print('WARNING: file for "{}" is missing'.format(tp))
+            audio = None
+        else:
+            audio = AudioSegment.from_wav(os.path.join(audios_dir, fname))
+            # NOTE: if file fails to load, pydub handles it by truncating audio data
+            if debug:
+                print('Length of raw bytes loaded:', len(audio.raw_data))
+        audios.append(audio)
+    df['Audio Clip'] = audios
+    print('Number of rows:', df.shape[0])
+    print('Filtering by mode: 3, 15, or 18')
+    df_filtered = df[df['Mode'].isin([3, 15, 18])]
+    print('Number of rows:', df_filtered.shape[0])
+
