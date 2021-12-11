534ca166f5d70aff7cad0d6b61c0b83fcbaf62eb
evelyn-crowe <evelyn.crowe@tamu.edu>
2021-12-10 18:38:15 -0600

    random forest with .3 test size- around 60% accuracy

--- data-cleaning-python/analysis.py
+++ data-cleaning-python/analysis.py
@@ -50,19 +50,22 @@ if __name__ == '__main__':
     print('Test accuracy:')
     print(s_test)
 
-    X_train= audios_array_1 # Features
-    y_train= labels_binary_1  # Labels
-    X_test = audios_array_2
-    y_test = labels_binary_2
+    from sklearn import metrics
+    # Model Accuracy, how often is the classifier correct?
+    
+    from sklearn.model_selection import train_test_split
+    X = np.concatenate((audios_array_1, audios_array_2))
+    y = np.concatenate((labels_binary_1, labels_binary_2))
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    from sklearn.model_selection import train_test_split
+    # X_train= audios_array_1 # Features
+    # y_train= labels_binary_1  # Labels
+    # X_test = audios_array_2
+    # y_test = labels_binary_2
     from sklearn.ensemble import RandomForestClassifier
     #Create a Gaussian Classifier
     clf=RandomForestClassifier(n_estimators=100)
     #Train the model using the training sets y_pred=clf.predict(X_test)
     clf.fit(X_train,y_train)
     y_pred=clf.predict(X_test)
-    print('Test accuracy: ')
-    print(y_pred)
-
-    from sklearn import metrics
-    # Model Accuracy, how often is the classifier correct?
     print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
